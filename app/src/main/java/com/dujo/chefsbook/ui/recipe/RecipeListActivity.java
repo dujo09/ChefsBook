b0bb8f9d43f7b15becf941a555476284ed05e8d0
Dujo Lelas <dujo.lelas09@gmail.com>
2025-11-07 01:41:58 +0100

    Auth buttons

--- app/src/main/java/com/dujo/chefsbook/ui/recipe/RecipeListActivity.java
+++ app/src/main/java/com/dujo/chefsbook/ui/recipe/RecipeListActivity.java
@@ -5,6 +5,8 @@ import static com.dujo.chefsbook.ui.addRecipe.AddRecipeActivity.EXTRA_RECIPE_ID;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -15,10 +17,15 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.dujo.chefsbook.R;
 import com.dujo.chefsbook.data.model.Recipe;
+import com.dujo.chefsbook.data.model.User;
 import com.dujo.chefsbook.ui.addRecipe.AddRecipeActivity;
+import com.dujo.chefsbook.ui.login.LoginActivity;
 import com.dujo.chefsbook.ui.recipeCategory.RecipeCategoryListActivity;
 import com.dujo.chefsbook.ui.recipeDetail.RecipeDetailActivity;
+import com.dujo.chefsbook.ui.register.RegisterActivity;
 import com.dujo.chefsbook.viewModel.RecipeViewModel;
+import com.dujo.chefsbook.viewModel.SharedUserViewModel;
+import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
@@ -36,13 +43,51 @@ public class RecipeListActivity extends AppCompatActivity {
     private Button btnAddRecipe;
     private String categoryId;
 
+    private Button btnLogin;
+    private Button btnRegister;
+    private Button btnLogout;
+    private SharedUserViewModel userViewModel;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_list);
 
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
         btnAddRecipe = findViewById(R.id.btnAddRecipe);
+        btnLogout = findViewById(R.id.btnLogout);
         tvTitle = findViewById(R.id.tvRecipesTitle);
+
+        btnRegister.setOnClickListener(v -> {
+            Intent i = new Intent(this, RegisterActivity.class);
+            startActivity(i);
+        });
+
+        btnLogout.setOnClickListener(v -> {
+            AuthUI.getInstance()
+                    .signOut(this)
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Signed out");
+                            userViewModel.clear();
+                        } else {
+                            Log.e(TAG, "Sign out failed", task.getException());
+                        }
+                    });
+        });
+
+        btnLogin.setOnClickListener(v -> {
+            Intent i = new Intent(this, LoginActivity.class);
+            startActivity(i);
+        });
+
+        btnAddRecipe.setOnClickListener(v -> {
+            Intent i = new Intent(this, AddRecipeActivity.class);
+            startActivity(i);
+        });
+
 
         rv = findViewById(R.id.rvRecipes);
         rv.setLayoutManager(new LinearLayoutManager(this));
@@ -59,6 +104,9 @@ public class RecipeListActivity extends AppCompatActivity {
             startActivity(i);
         });
 
+        userViewModel = new ViewModelProvider(this).get(SharedUserViewModel.class);
+        userViewModel.getUser().observe(this, this::updateUi);
+
         categoryId = getIntent().getStringExtra(RecipeCategoryListActivity.EXTRA_CATEGORY_ID);
         String categoryName = getIntent().getStringExtra(RecipeCategoryListActivity.EXTRA_CATEGORY_NAME);
         if (categoryName != null) tvTitle.setText(categoryName);
@@ -69,5 +117,27 @@ public class RecipeListActivity extends AppCompatActivity {
         recipeViewModel.getFilteredRecipes().observe(this, recipes -> {
             if (recipes != null) recipeAdapter.submitList(recipes);
         });
+    }
+
+    private void updateUi(User user) {
+        if (user != null) {
+            btnRegister.setVisibility(View.GONE);
+            btnRegister.setEnabled(false);
+            btnAddRecipe.setVisibility(View.VISIBLE);
+            btnAddRecipe.setEnabled(true);
+            btnLogin.setVisibility(View.GONE);
+            btnLogin.setEnabled(false);
+            btnLogout.setVisibility(View.VISIBLE);
+            btnLogout.setEnabled(true);
+        } else {
+            btnRegister.setVisibility(View.VISIBLE);
+            btnRegister.setEnabled(true);
+            btnAddRecipe.setVisibility(View.GONE);
+            btnAddRecipe.setEnabled(false);
+            btnLogin.setVisibility(View.VISIBLE);
+            btnLogin.setEnabled(true);
+            btnLogout.setVisibility(View.GONE);
+            btnLogout.setEnabled(false);
+        }
     }
 }
--- app/src/main/java/com/dujo/chefsbook/ui/recipeCategory/RecipeCategoryListActivity.java
+++ app/src/main/java/com/dujo/chefsbook/ui/recipeCategory/RecipeCategoryListActivity.java
@@ -2,6 +2,8 @@ package com.dujo.chefsbook.ui.recipeCategory;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,8 +22,10 @@ import com.dujo.chefsbook.data.model.User;
 import com.dujo.chefsbook.ui.addRecipe.AddRecipeActivity;
 import com.dujo.chefsbook.ui.login.LoginActivity;
 import com.dujo.chefsbook.ui.recipe.RecipeListActivity;
+import com.dujo.chefsbook.ui.register.RegisterActivity;
 import com.dujo.chefsbook.viewModel.RecipeCategoryViewModel;
 import com.dujo.chefsbook.viewModel.SharedUserViewModel;
+import com.firebase.ui.auth.AuthUI;
 
 public class RecipeCategoryListActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -29,8 +33,10 @@ public class RecipeCategoryListActivity extends AppCompatActivity {
     public static final String EXTRA_CATEGORY_NAME = "extra:category_name";
 
     private TextView tvStatus;
-    private Button btnSignOut;
+    private Button btnLogin;
     private Button btnAddRecipe;
+    private Button btnRegister;
+    private Button btnLogout;
 
     private RecipeCategoryViewModel recipeCategoryViewModel;
     private RecipeCategoryAdapter recipeCategoryAdapter;
@@ -48,22 +54,32 @@ public class RecipeCategoryListActivity extends AppCompatActivity {
         });
 
         tvStatus = findViewById(R.id.tvStatus);
-        btnSignOut = findViewById(R.id.btnSignOut);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
         btnAddRecipe = findViewById(R.id.btnAddRecipe);
+        btnLogout = findViewById(R.id.btnLogout);
 
-        btnSignOut.setOnClickListener(v -> {
+        btnRegister.setOnClickListener(v -> {
+            Intent i = new Intent(this, RegisterActivity.class);
+            startActivity(i);
+        });
+
+        btnLogout.setOnClickListener(v -> {
+            AuthUI.getInstance()
+                    .signOut(this)
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Signed out");
+                            userViewModel.clear();
+                        } else {
+                            Log.e(TAG, "Sign out failed", task.getException());
+                        }
+                    });
+        });
+
+        btnLogin.setOnClickListener(v -> {
             Intent i = new Intent(this, LoginActivity.class);
             startActivity(i);
-//            AuthUI.getInstance()
-//                    .signOut(this)
-//                    .addOnCompleteListener(task -> {
-//                        if (task.isSuccessful()) {
-//                            Log.d(TAG, "Signed out");
-//                            userViewModel.clear();
-//                        } else {
-//                            Log.e(TAG, "Sign out failed", task.getException());
-//                        }
-//                    });
         });
 
         btnAddRecipe.setOnClickListener(v -> {
@@ -97,8 +113,24 @@ public class RecipeCategoryListActivity extends AppCompatActivity {
     private void updateUi(User user) {
         if (user != null) {
             tvStatus.setText(user.username);
+            btnRegister.setVisibility(View.GONE);
+            btnRegister.setEnabled(false);
+            btnAddRecipe.setVisibility(View.VISIBLE);
+            btnAddRecipe.setEnabled(true);
+            btnLogin.setVisibility(View.GONE);
+            btnLogin.setEnabled(false);
+            btnLogout.setVisibility(View.VISIBLE);
+            btnLogout.setEnabled(true);
         } else {
             tvStatus.setText("");
+            btnRegister.setVisibility(View.VISIBLE);
+            btnRegister.setEnabled(true);
+            btnAddRecipe.setVisibility(View.GONE);
+            btnAddRecipe.setEnabled(false);
+            btnLogin.setVisibility(View.VISIBLE);
+            btnLogin.setEnabled(true);
+            btnLogout.setVisibility(View.GONE);
+            btnLogout.setEnabled(false);
         }
     }
 }
